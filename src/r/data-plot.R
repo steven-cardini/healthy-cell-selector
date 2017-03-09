06d86235e0c9a54ec6f232b4cdf98fd8b2a1e948
Steven Cardini <stevenrene.cardini@students.bfh.ch>
2017-03-09 20:09:41 +0100

    facilitates R functions

--- src/r/data-plot.R
+++ src/r/data-plot.R
@@ -3,6 +3,7 @@ require(dplyr)
 
 require(rpart)
 require(rpart.plot)
+require(RWeka)
 
 require(ggplot2)
 require(plotly)
@@ -16,25 +17,12 @@ cell.id.arg <- 'cell_Id'
 cell.id.args.vec <- c('Image_Metadata_Site', 'Well', 'objNuc_TrackObjects_Label')
 cell.metadata.args.vec <- c('Stimulation_duration', 'Stimulation_intensity', 'Stimulation_treatment')
 cell.class.arg <- 'mid.in'
+cell.feature.prefix <- 'objCell_'
 
 plot.x.arg <- 'objNuc_Intensity_MeanIntensity_imNucCorrBg'
 plot.y.arg <- 'objNuc_Intensity_MeanIntensity_imErkCorrOrig + objCyto_Intensity_MeanIntensity_imErkCorrOrig'
 plot.group.arg <- cell.id.arg
 plot.color.arg <- cell.class.arg
-
-decisiontree.args.vec <- c('objCell_AreaShape_Area', 'objCell_AreaShape_Compactness', 'objCell_AreaShape_Eccentricity',
-                           'objCell_AreaShape_EulerNumber', 'objCell_AreaShape_Extent', 'objCell_AreaShape_FormFactor',
-                           'objCell_AreaShape_MajorAxisLength', 'objCell_AreaShape_MaxFeretDiameter', 'objCell_AreaShape_MaximumRadius',
-                           'objCell_AreaShape_MeanRadius', 'objCell_AreaShape_MedianRadius', 'objCell_AreaShape_MinFeretDiameter',
-                           'objCell_AreaShape_MinorAxisLength', 'objCell_AreaShape_Orientation', 'objCell_AreaShape_Perimeter',
-                           'objCell_AreaShape_Solidity', 'objCell_Neighbors_AngleBetweenNeighbors_25', 
-                           'objCell_Neighbors_FirstClosestDistance_25', 'objCell_Neighbors_FirstClosestObjectNumber_25',
-                           'objCell_Neighbors_NumberOfNeighbors_25', 'objCell_Neighbors_PercentTouching_25',
-                           'objCell_Neighbors_SecondClosestDistance_25', 'objCell_Neighbors_SecondClosestObjectNumber_25')
-
-
-
-# setdiff(names(cells.data)[names(cells.data) %like% 'objCell'], c('objCell_AreaShape_Compactness'))
 
 scale.exlude.args.vec <- c('objCell_AreaShape_EulerNumber', 'objCell_Neighbors_FirstClosestObjectNumber_25',
                            'objCell_Neighbors_NumberOfNeighbors_25', 'objCell_Neighbors_PercentTouching_25',
@@ -44,27 +32,30 @@ scale.exlude.args.vec <- c('objCell_AreaShape_EulerNumber', 'objCell_Neighbors_F
 # import data from file and add cell IDs
 
 dat <- fread("data/tCoursesSelected_midInCol.csv")
+str(dat)
 dat <- addCellIds(dat, cell.id.arg, cell.id.args.vec)
 
 # create data and metadata subsets for further analysis
 
-cells.data <- getCellData(dat, cell.id.args.vec, cell.metadata.args.vec)
-cells.metadata <- getCellMetadata(dat, cell.id.arg, cell.id.args.vec, cell.metadata.args.vec)
+cells.data <- dplyr::select(dat, -dplyr::one_of(cell.id.args.vec, cell.metadata.args.vec))
+cells.metadata <- dplyr::select(dat, dplyr::one_of(cell.id.arg, cell.class.arg, cell.id.args.vec, cell.metadata.args.vec)) %>%
+  dplyr::distinct()
 rm(dat) # delete raw data
 
-cells.averageData <- getAverageCellData(cells.data, cell.id.arg, cell.class.arg, decisiontree.args.vec)
-cells.averageData.scaled <- getScaledData(cells.averageData, scale.exlude.args.vec)
+cells.dt.data <- dplyr::select(cells.data, one_of(cell.id.arg, cell.class.arg), starts_with(cell.feature.prefix))
+cells.dt.avgData <- getAverageCellData(cells.dt.data, cell.id.arg, cell.class.arg)
+cells.dt.avgData.scaled <- getScaledData(cells.dt.avgData, scale.exlude.args.vec)
+
 
 # Decision tree
 # http://data-mining.business-intelligence.uoc.edu/home/j48-decision-tree
 # https://en.wikibooks.org/wiki/Data_Mining_Algorithms_In_R/Packages/RWeka/Weka_classifier_trees
 
-dt <- rpart(mid.in~., data=cells.averageData.scaled)
+dt <- rpart(mid.in~., data=cells.dt.avgData.scaled)
 prp(dt)
 summary(dt)
 
-# require(RWeka)
-# dt2 <- J48(mid.in~., data=cells.averageData)
+# dt2 <- J48(mid.in~., data=cells.dt.avgData.scaled)
 # summary(dt2)
 
 
--- src/r/functions.R
+++ src/r/functions.R
@@ -4,8 +4,8 @@ require(dplyr)
 addCellIds = function (in.data, in.cell.id.arg, in.cell.id.args.vec) {
   
   # add cell IDs to data set
-  loc.dat.temp <- dplyr::select(in.data, dplyr::one_of(in.cell.id.args.vec))
-  loc.dat.temp <- dplyr::distinct(loc.dat.temp)
+  loc.dat.temp <- dplyr::select(in.data, dplyr::one_of(in.cell.id.args.vec)) %>%
+    dplyr::distinct()
   loc.dat.temp[,in.cell.id.arg] <- 1:nrow(loc.dat.temp)
   
   out.data <- dplyr::inner_join(loc.dat.temp, in.data, by = in.cell.id.args.vec) # now equals input data set plus new row in.cell.id.arg
@@ -13,39 +13,22 @@ addCellIds = function (in.data, in.cell.id.arg, in.cell.id.args.vec) {
 }
 
 
-getCellData = function (in.data, in.cell.id.args.vec, in.cell.metadata.args.vec) {
-  
-  # remove cell id group columns as well as metadata columns
-  out.data <- dplyr::select(in.data, -dplyr::one_of(in.cell.id.args.vec, in.cell.metadata.args.vec)) 
-  
-  return(out.data)
-}
-
-
-getCellMetadata = function (in.data, in.cell.id.arg, in.cell.id.args.vec, in.cell.metadata.args.vec) {
-  
-  out.data <- dplyr::select(in.data, dplyr::one_of(in.cell.id.arg, in.cell.id.args.vec, in.cell.metadata.args.vec))
-  out.data <- dplyr::distinct(out.data)
-  
-  return(out.data)
-}
-
-
-getAverageCellData = function (in.data, in.cell.id.arg, in.cell.class.arg, in.select.args.vec) {
+getAverageCellData = function (in.data, in.cell.id.arg, in.cell.class.arg) {
   
   # group data by cell id and average other attributes, return only relevant attributes
   out.data <- in.data %>%
                 group_by_(in.cell.id.arg, in.cell.class.arg) %>%
                 summarise_each(funs(mean)) %>%
                 ungroup() %>%
-                select(one_of(in.cell.class.arg, in.select.args.vec))
+                select(-get(in.cell.id.arg))
   
   return(out.data)
 }
 
+
 getScaledData = function (in.data, in.scale.exlude.args.vec) {
   
-  temp.index = match(in.scale.exlude.args.vec, names(in.data))
+  temp.index <- match(in.scale.exlude.args.vec, names(in.data))
   out.data <- in.data
   out.data[,-c(temp.index)] <- scale(out.data[,-c(temp.index)])
   
